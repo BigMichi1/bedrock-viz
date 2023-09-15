603f73bf5312b886bfa20cfe1ebbc84943bb707a
Michael Cramer <michael@bigmichi1.de>
2023-09-15 13:51:13 +0200

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-patch -N -p0 < patches/leveldb-1.22.patch
-patch -N -p0 < patches/libnbtplusplus-2.5.patch
-patch -N -p0 < patches/pugixml-1.14.patch
+# Function to apply patches
+apply_patches() {
+    patch -N -p0 < patches/leveldb-1.22.patch
+    patch -N -p0 < patches/libnbtplusplus-2.5.patch
+    patch -N -p0 < patches/pugixml-1.14.patch
+}
 
-rm -rf build
-mkdir -p build
-cd build
+# Function to build and install
+build_and_install() {
+    rm -rf build
+    mkdir -p build
+    cd build
 
-cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS= -DCMAKE_C_FLAGS= ..
-make
-make install
+    cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS= -DCMAKE_C_FLAGS= ..
+
+    make
+
+    if [ "$INSTALL" = true ]; then
+        make install
+    else
+        echo "Skipping 'make install' as requested."
+    fi
+}
+
+# Check if the script is called with "--no-install" option
+if [[ "$1" == "--no-install" ]]; then
+    INSTALL=false
+else
+    INSTALL=true
+fi
+
+apply_patches
+build_and_install
